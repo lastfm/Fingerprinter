80af543140ffa6d17cb401e99fe1df70cb5e08db
Norman Casagrande <nova77@gmail.com>
2007-10-05 10:20:28 +0000

    fixed some comments

--- lastfmfpclient/src/Sha256File.cpp
+++ lastfmfpclient/src/Sha256File.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2005 - 2007 by                                          *
- *      Last.fm Ltd <mir@last.fm>                                       *
+ *      Last.fm Ltd <mir@last.fm>                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
