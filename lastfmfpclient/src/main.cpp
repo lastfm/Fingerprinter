ad966462ffe90d418cd61bfc77cb8624651d44b7
Norman Casagrande <nova77@gmail.com>
2007-11-07 11:48:20 +0000

    versioning

--- lastfmfpclient/src/main.cpp
+++ lastfmfpclient/src/main.cpp
@@ -54,7 +54,7 @@ using namespace std;
 // DO NOT CHANGE THOSE!
 const char FP_SERVER_NAME[]       = "ws.audioscrobbler.com/fingerprint/query/";
 const char METADATA_SERVER_NAME[] = "ws.audioscrobbler.com/fingerprint/fp.php";
-const char PUBLIC_CLIENT_NAME[]   = "FP Beta 1.33";
+const char PUBLIC_CLIENT_NAME[]   = "FP Beta 1.34";
 const char HTTP_POST_DATA_NAME[]  = "fpdata";
 
 // -----------------------------------------------------------------------------
