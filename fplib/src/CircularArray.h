7ed3cffe26823ed40ebc6a3a7b3562effeb99061
Norman Casagrande <nova77@gmail.com>
2010-06-29 11:57:26 +0000

    updated license info

--- fplib/src/CircularArray.h
+++ fplib/src/CircularArray.h
@@ -1,21 +1,20 @@
 /***************************************************************************
- *   Copyright (C) 2005 - 2007 by                                          *
- *      Last.fm Ltd <mir@last.fm>                                          *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+* This file is part of last.fm fingerprint app                             *
+*  Last.fm Ltd <mir@last.fm>                                               *
+*                                                                          *
+* The last.fm fingerprint app is free software: you can redistribute it    *
+* and/or modify it under the terms of the GNU General Public License as    *
+* published by the Free Software Foundation, either version 3 of the       *
+* License, or (at your option) any later version.                          *
+*                                                                          *
+* The last.fm fingerprint app and library are distributed in the hope that *
+* it will be useful, but WITHOUT ANY WARRANTY; without even the implied    *
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See    *
+* the GNU General Public License for more details.                         *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with The last.fm fingerprint.                                      *
+* If not, see <http://www.gnu.org/licenses/>.                              *
  ***************************************************************************/
 
 #ifndef __CIRCULAR_ARRAY_H
--- fplib/src/OptFFT.h
+++ fplib/src/OptFFT.h
@@ -1,27 +1,26 @@
 /***************************************************************************
- *   Copyright (C) 2005 - 2007 by                                          *
- *      Last.fm Ltd <mir@last.fm>                                          *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
- *                                                                         *
- *   Part of this code is based on the work of Y. Ke, D. Hoiem, and        *
- *   R. Sukthankar - "Computer Vision for Music Identification",           *
- *   in Proceedings of Computer Vision and Pattern Recognition, 2005.      *
- *   See also http://www.cs.cmu.edu/~yke/musicretrieval/                   *
- ***************************************************************************/
+* This file is part of last.fm fingerprint app                             *
+*  Last.fm Ltd <mir@last.fm>                                               *
+*                                                                          *
+* The last.fm fingerprint app is free software: you can redistribute it    *
+* and/or modify it under the terms of the GNU General Public License as    *
+* published by the Free Software Foundation, either version 3 of the       *
+* License, or (at your option) any later version.                          *
+*                                                                          *
+* The last.fm fingerprint app and library are distributed in the hope that *
+* it will be useful, but WITHOUT ANY WARRANTY; without even the implied    *
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See    *
+* the GNU General Public License for more details.                         *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with The last.fm fingerprint.                                      *
+* If not, see <http://www.gnu.org/licenses/>.                              *
+*                                                                          *
+* Part of this code is based on the work of Y. Ke, D. Hoiem, and           *
+* R. Sukthankar - "Computer Vision for Music Identification",              *
+* in Proceedings of Computer Vision and Pattern Recognition, 2005.         *
+* See also http://www.cs.cmu.edu/~yke/musicretrieval/                      *
+***************************************************************************/
 
 #ifndef __OPT_FFT_H
 #define __OPT_FFT_H
